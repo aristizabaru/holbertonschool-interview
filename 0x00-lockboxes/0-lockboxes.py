9cd50a2e6bb1020a2e8cd560825a1002ce007a9d
Andrés Aristizábal <andres.aristizabal@gmail.com>
2021-07-22 17:56:04 -0500

    chore: change entire algorithm

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,25 +5,40 @@
 def canUnlockAll(boxes):
     """You have `n` number of locked boxes in front of you.
     Each box is numbered sequentially from `0` to `n - 1`
-    and each box may contain keys to the other boxes.
+    and each box may contain keychain to the other boxes.
 
     This function determines if all the boxes can be opened.
 
     * The first box boxes[0] is unlocked.
 
     Args:
-        boxes (list): list of list containing keys
+        boxes (list): list of list containing keychain
 
     Returns:
         Bool: True if all boxes can be unlocked. False if fail
     """
-    keys = set()
-
+    keychain = set()
     for next_key, box in enumerate(boxes):
         next_key += 1
-        keys.update(box)
         for key in box:
-            keys.update(boxes[key])
-        if next_key not in keys and next_key < len(boxes):
-            return False
-    return True
+            if key < len(boxes):
+                keychain.add(key)
+                keychain.update(boxes[key])
+            if length(keychain, boxes):
+                return True
+        if next_key not in keychain:
+            break
+    return False
+
+
+def length(keychain, boxes):
+    """compare lengths of two lists
+
+    Args:
+        keychain (list): keychain of boxes
+        boxes (list): boxes to be oppened
+
+    Returns:
+        Bool: True if equal, False if not
+    """
+    return len(keychain) == len(boxes) or len(keychain) == len(boxes) - 1
