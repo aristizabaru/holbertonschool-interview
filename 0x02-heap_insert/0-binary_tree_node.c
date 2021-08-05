e9a4db1c99fa921ded1e50bcd0a6f2ef64b775c4
Andrés Aristizábal <andres.aristizabal@gmail.com>
2021-08-04 21:24:36 -0500

    refactor: remove extra function create_node() as it can be done inside binary_tree_node function

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -11,42 +11,12 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = NULL;
 
-	/* tree is empty */
-	if (!parent)
-	{
-		new_node = create_node();
-		new_node->n = value;
-		return (new_node);
-	}
-	/* parent is valid */
-	if (parent)
-	{
-		new_node = create_node();
-		if (value)
-			new_node->n = value;
-		new_node->parent = parent;
-		return (new_node);
-	}
-	return (NULL);
-}
-
-/**
- * create_node - create a new empty binary_tree_t
- * 
- * Return: pointer to new binary_tree_t
- */
-binary_tree_t *create_node(void)
-{
-	binary_tree_t *new_node = NULL;
-
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
-
-	new_node->n = 0;
-	new_node->parent = NULL;
+	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
-
+	new_node->parent = parent;
 	return (new_node);
 }
