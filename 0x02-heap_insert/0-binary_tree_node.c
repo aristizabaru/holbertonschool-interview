c5e6d8e56a93039c217869593b79968f67d33691
Andrés Aristizábal <andres.aristizabal@gmail.com>
2021-08-04 21:30:17 -0500

    style: remove blanck space from docstring

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -4,7 +4,7 @@
  * binary_tree_node - create a new binary tree node
  * @parent: pointer to the new binary tree's parent node
  * @value: integer to be save in the new binary tree node
- * 
+ *
  * Return: pointer to new binary tree node
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
